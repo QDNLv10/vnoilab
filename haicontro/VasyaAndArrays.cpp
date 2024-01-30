b9cf6053a476bed1a4371178791a8a8a63b559a0
SaoYui <dusqdn.eml@gmail.com>
2024-01-30 15:42:19 +0700

    Add + Done: VasyaAndArrays

---
+++ haicontro/VasyaAndArrays.cpp
@@ -0,0 +1,48 @@
+// https://codeforces.com/problemset/problem/1036/D
+
+void solve();
+int main() {
+  solve();
+  return 0;
+}
+
+#include <cstdio>
+#include <vector>
+
+#define Array std::vector<int>
+#define INT long long
+
+void inpArr(Array &a, int n) {
+  for (int i = 0; i < n; ++i) {
+    INT temp;
+    scanf("%lld", &temp);
+    a.push_back(temp);
+  }
+}
+
+void solve() {
+  INT m, n;
+  Array a, b;
+  scanf("%lld", &m);
+  inpArr(a, m);
+  scanf("%lld", &n);
+  inpArr(b, n);
+
+  INT sum = 0;
+  INT count = 0;
+  INT ia = 0, ib = 0;
+  while (ia < m || ib < n) {
+    if (ib == n || (ia < m && sum <= 0))
+      sum += a[ia++];
+    else
+      sum -= b[ib++];
+
+    if (sum == 0)
+      ++count;
+  }
+
+  if (count < 1 || sum != 0)
+    printf("-1\n");
+  else
+    printf("%lld\n", count);
+}
