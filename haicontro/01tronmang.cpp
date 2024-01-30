48a3628f0297496003c2bd2f2571b6c919e9210a
SaoYui <dusqdn.eml@gmail.com>
2024-01-30 09:44:37 +0700

    Init Again

---
+++ haicontro/01tronmang.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <vector>
+
+#define Array std::vector<int>
+
+void tronMang(const Array &a, const Array &b) {
+  int ia = 0, ib = 0;
+  int na = a.size(), nb = b.size();
+  Array c;
+  while (ia < na || ib < nb) {
+    if (ib == nb || (ia < na && a[ia] < b[ib]))
+      c.push_back(a[ia++]);
+    else
+      c.push_back(b[ib++]);
+  }
+
+  for (auto val : c)
+    printf("%d ", val);
+  printf("\n");
+}
+
+int main() { tronMang(Array{2, 2, 3, 5, 7, 8}, Array{1, 3, 4, 5, 6, 9}); }
