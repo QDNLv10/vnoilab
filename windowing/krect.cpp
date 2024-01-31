a299a5d6e6c3355296392483d28cf5fabc93d938
SaoYui <dusqdn.eml@gmail.com>
2024-01-31 09:06:37 +0700

    Done: windowing/krect.cpp

--- windowing/krect.cpp
+++ windowing/krect.cpp
@@ -9,21 +9,24 @@ int main() {
 
 #include <cstdio>
 #include <iostream>
-#include <map>
 #include <string>
 #include <vector>
 
 #define INT long long
 #define Array std::vector<std::string>
 
-#define Map std::map<char, INT>
+#define Map std::vector<INT>
 
 INT incVal(Map &b, char key, INT val) {
-  if (b.find(key) == b.end()) {
-    b[key] = 0;
+  b[key - 'A'] += val;
+  return b[key - 'A'];
+}
+
+void clearMap(Map &b) {
+  b.clear();
+  for (char c = 'A'; c <= 'Z'; ++c) {
+    b.push_back(0);
   }
-  b[key] += val;
-  return b[key];
 }
 
 void solve() {
@@ -38,12 +41,12 @@ void solve() {
   }
 
   INT ret = 0;
-  for (INT ixb = 0; ixb < n; ++ixb) {
-    for (INT ixa = 0; ixa <= ixb; ++ixa) {
+  for (INT ixa = 0; ixa < n; ++ixa) {
+    for (INT ixb = ixa; ixb < n; ++ixb) {
       INT iya1 = 0, iya2 = 0, count1 = 0, count2 = 0;
       INT ret1 = 0, ret2 = 0;
-      b1.clear();
-      b2.clear();
+      clearMap(b1);
+      clearMap(b2);
       for (INT iyb = 0; iyb < m; ++iyb) {
 
         for (INT i = ixa; i <= ixb; ++i) {
